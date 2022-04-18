0aed1a6987829464a295f8894b8d8f39f96286f4
SmritiJaiswal12 <smriti.jaiswal03@outlook.com>
2022-04-18 19:58:56 +0530

    first commit

---
+++ PracticeCheck/Controllers/MenuItemController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeCheck.Controllers
+{
+    public class MenuItemController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            return new List<MenuItem> { new MenuItem()
+            {
+                Id = 1,
+                Name = "Chicken",
+                Price = 150,
+                Active = true,
+                DateOfLaunch = Convert.ToDateTime("2002-12-12 12:12:00.0000000"),
+                FreeDelivery = true
+            },
+            new MenuItem()
+            {
+                 Id = 2,
+                 Name = "Tomato Soup",
+                 Price = 50,
+                 Active = true,
+                 DateOfLaunch = Convert.ToDateTime("2002-12-12 12:12:00.0000000"),
+                 FreeDelivery = true
+            }
+            };
+        }
+    }
+}
